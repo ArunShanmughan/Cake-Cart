0f00ab34c61ecaa7d5d953b94b26ed56322ea100
Arun <arunshanmughan35@gmail.com>
2024-04-09 18:06:22 +0530

    done product editing successfully

---
+++ controllers/productcontroller.js
@@ -0,0 +1,154 @@
+const userdata = require("../models/userDB");
+const categoryModel = require("../models/categoryModel");
+const productModel = require("../models/productModel");
+
+const getProductManagment = async (req, res) => {
+  try {
+    if (req.session.adminLogged) {
+      const productdetails = await productModel.find().populate("category");
+      // const categorydetails = await categoryModel.find({
+      //   _id: productdetails._id,
+      // })
+      res.render("admin/productManagment", {
+        productData: productdetails,
+        // categoryModel: categorydetails,
+      });
+    }
+  } catch (error) {
+    console.log("Something Went Wrong", error);
+  }
+};
+
+
+const productBlock = async (req, res) => {
+  try {
+    await productModel.updateOne(
+      { _id: req.query.id },
+      { $set: { isListed: false } }
+    );
+    res.send({ block: true });
+  } catch (error) {
+    console.log("Something Went Wrong", error);
+  }
+};
+
+
+const productUnBlock = async (req, res) => {
+  try {
+    await productModel.updateOne(
+      { _id: req.query.id },
+      { $set: { isListed: true } }
+    );
+    res.send({ unbBlock: true });
+  } catch (error) {
+    console.log("Something Went Wrong", error);
+  }
+};
+
+
+const getAddProduct = async (req, res) => {
+  try {
+    if (req.session.adminLogged) {
+      const categorydetails = await categoryModel.find();
+      res.render("admin/addproduct", { categoryModel: categorydetails });
+    }
+  } catch (error) {
+    console.log("Something Went Wrong", error);
+  }
+};
+
+
+const postAddProduct = async(req,res)=>{
+  // console.log(req.files)
+  try {
+    if(req.session.adminLogged){
+    let images = [];
+    for(let i=0;i<req.files.length;i++){
+      images[i]=req.files[i].filename;
+    }
+    // console.log(images)
+    // console.log(req.body);
+    const addproduct = new productModel({
+      productName: req.body.productName,
+      category: req.body.parentCategory,
+      images: images,
+      price: req.body.productPrice,
+      quantity: req.body.productQuantity
+  });
+  const productDetails = await productModel.find({ productName: { $regex: new RegExp('^' + req.body.productName.toLowerCase() + '$', 'i') } })
+        if (/^\s*$/.test(req.body.productName) || /^\s*$/.test(req.body.productPrice) || /^\s*$/.test(req.body.productStock)) {
+            res.send({ noValue: true })
+        }
+        else if (productDetails.length > 0) {
+            res.send({ exists: true })
+        } else {
+            res.send({ success: true })
+            addproduct.save()
+        }
+      }
+  } catch (error) {
+    console.log("Something Went Wrong",error)
+  }
+}
+
+
+const getEditProduct = async(req,res)=>{
+  try {
+    if(req.session.adminLogged){
+      const categoryDetails = await categoryModel.find()
+      const categoryInfo = await categoryModel.findOne({ _id: req.query.catId })
+      const productInfo = await productModel.findOne({ _id: req.query.proId })
+    res.render("admin/editProduct",{productInfo:productInfo,categoryInfo:categoryInfo,categorydetails:categoryDetails});
+    }
+  } catch (error) {
+    console.log("Something Went Wrong",error);
+  }
+}
+
+
+const postEditProduct = async(req,res)=>{
+  try {
+    if(req.files.length==0){
+      const existingProduct = await productModel.findOne({_id:req.params.id});
+      var imageFiles = existingProduct.images;
+    }else if(req.files.length<3){
+      res.send({noImage:true})
+    }else{
+    var imageFiles = [];
+    for(let i=0;i<req.files.length;i++){
+      imageFiles[i]=req.files[i].filename;
+      }
+    }
+    const productDetails = await productModel.find({_id:{$ne:req.params.id},productName: { $regex: new RegExp('^' + req.body.productName.toLowerCase() + '$', 'i') } })
+        if (/^\s*$/.test(req.body.productName) || /^\s*$/.test(req.body.productPrice) || /^\s*$/.test(req.body.productStock)) {
+            res.send({ noValue: true })
+        }else if (productDetails.length > 0 && req.body.productName != productDetails.productName ) {
+          res.send({ exists: true })
+      } else {
+          await productModel.updateOne({ _id: req.params.id }, {
+              $set: {
+                  productName: req.body.productName,
+                  category: req.body.parentCategory,
+                  Images: imageFiles,
+                  price: req.body.productPrice,
+                  quantity: req.body.productStock
+              }
+          })
+          res.send({ success: true })
+
+      }
+  } catch (error) {
+    console.log("Something Went Wrong",error)
+  }
+}
+
+
+module.exports={
+  getProductManagment,
+  productBlock,
+  productUnBlock,
+  getAddProduct,
+  postAddProduct,
+  getEditProduct,
+  postEditProduct
+}
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,45 +1,58 @@
 const express = require("express");
 const admRouter = express.Router();
-const upload = require('../services/multer.js')
+const upload = require("../services/multer.js");
 
-const {getAdmin,
-      postAdmin,
-      getAdminDash,
-      getUserManagment,
-      userBlock,
-      userUnBlock,
-      getCategoryManagment,
-      getProductManagment,
-      categoryList,
-      categoryUnList,
-      getAddCategory,
-      postEditCategory,
-      productBlock,
-      productUnBlock,
-      getAddProduct,
-      postAddProduct,
-      getEditProduct,
-      postEditProduct} = require("../controllers/admincontroller")
+const {
+  getAdmin,
+  postAdmin,
+  getAdminDash,
+  getUserManagment,
+  userBlock,
+  userUnBlock,
+  admLogout,
+} = require("../controllers/admincontroller");
 
-admRouter.get("/admin",getAdmin);
-admRouter.post("/adminlog",postAdmin);
-admRouter.get("/admindash",getAdminDash);
-admRouter.get("/usersmanagment",getUserManagment);
-admRouter.get("/userBlock",userBlock);
-admRouter.get("/userUnBlock",userUnBlock);
-admRouter.get("/categoryManagment",getCategoryManagment)
-admRouter.get("/productManagment",getProductManagment)
-admRouter.get("/categoryunlist",categoryList);
-admRouter.get("/categorylist",categoryUnList);
-admRouter.post("/addCategory",getAddCategory);
-admRouter.post("/editCategory",postEditCategory);
-admRouter.get("/productBlock",productBlock);
-admRouter.get("/productUnBlock",productUnBlock);
-admRouter.get("/addProduct",getAddProduct);
-admRouter.post("/addProducts",upload.any(),postAddProduct);
-admRouter.get("/editProduct",getEditProduct);
-admRouter.post("/editProducts",upload.any(),postEditProduct)
+const {
+  getCategoryManagment,
+  categoryList,
+  categoryUnList,
+  getAddCategory,
+  postEditCategory,
+} = require("../controllers/categorycontroller.js");
 
+const {
+  getProductManagment,
+  productBlock,
+  productUnBlock,
+  getAddProduct,
+  postAddProduct,
+  getEditProduct,
+  postEditProduct,
+} = require("../controllers/productcontroller.js");
 
+//admin Routers
+admRouter.get("/admin", getAdmin);
+admRouter.post("/adminlog", postAdmin);
+admRouter.get("/admindash", getAdminDash);
+admRouter.get("/usersmanagment", getUserManagment);
+admRouter.get("/userBlock", userBlock);
+admRouter.get("/userUnBlock", userUnBlock);
+admRouter.get("/admLogOut",admLogout)
+
+//Category Routers
+admRouter.get("/categoryManagment", getCategoryManagment);
+admRouter.get("/categoryunlist", categoryList);
+admRouter.get("/categorylist", categoryUnList);
+admRouter.post("/addCategory", getAddCategory);
+admRouter.post("/editCategory", postEditCategory);
+
+//Product Routers
+admRouter.get("/productManagment", getProductManagment);
+admRouter.get("/productBlock", productBlock);
+admRouter.get("/productUnBlock", productUnBlock);
+admRouter.get("/addProduct", getAddProduct);
+admRouter.post("/addProducts", upload.any(), postAddProduct);
+admRouter.get("/editProduct", getEditProduct);
+admRouter.post("/editProducts/:id", upload.any(), postEditProduct);
 
 module.exports = admRouter;
